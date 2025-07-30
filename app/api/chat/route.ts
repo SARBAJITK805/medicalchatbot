b7f99aaa179f70990e95a7266c538b43a9960811
SARBAJITK805 <sarbajitk8@gmail.com>
2025-07-31 01:33:14 +0530

    a bit of fix

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -90,7 +90,7 @@ export async function POST(req: Request) {
         ];
 
         const selectModel = genAI.getGenerativeModel({ 
-            model: "gemini-1.5-flash",
+            model: "gemini-2.5-flash",
             generationConfig: {
                 temperature: 0.7,
                 topP: 0.8,
